5001435155ec9539bdadb93cd4ec49039140996d
天韵 施 <77976248@qq.com>
2017-12-13 13:20:11 +0800

    添加 action state
    之前unity编译报错是因为 action.cs 和框架自带的命名冲突

--- Assets/Scripts/AI/State.cs
+++ Assets/Scripts/AI/State.cs
@@ -1,21 +1,21 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//
-//[CreateAssetMenu(menuName = "PluggableAI/State")]
-//public class State : ScriptableObject {
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "PluggableAI/State")]
+public class State : ScriptableObject {
 
 
-//	public Action[] actions;
-//
-//	public Color sceneGizmoColor = Color.grey;
-//	public void UpdateState(StateController controller){
-//		DoAction (controller);
-//	}
-//
-//	private void DoAction(StateController controller){
-//		for (int i = 0; i < actions.Length; i++) {
-//			actions [i].Act (controller);
-//		}
-//	}
-//}
+	public Actions[] actions;
+
+	public Color sceneGizmoColor = Color.grey;
+	public void UpdateState(StateController controller){
+		DoAction (controller);
+	}
+
+	private void DoAction(StateController controller){
+		for (int i = 0; i < actions.Length; i++) {
+			actions [i].Act (controller);
+		}
+	}
+}
